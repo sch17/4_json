e9f57ab83c4ecd262056543d05f57280dd3cf284
sch17 <scherbakovn@gmail.com>
2017-09-22 15:10:53 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -7,8 +7,8 @@ def load_json_file(filepath):
     return json_data
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4))
+def pretty_print_json(json_data):
+    print(json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
